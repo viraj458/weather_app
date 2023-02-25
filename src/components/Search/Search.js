b0f274c63d57de9263544a56aebb9bac282162d4
Viraj <virajmalaka94@gmail.com>
2023-02-26 02:23:42 +0530

    created Search.css

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import './Search.css'
+import { FaSearch } from "react-icons/fa";
 
 const Search = ({setData}) => {
     const [latitude, setLatitude] = useState('')
@@ -7,7 +9,7 @@ const Search = ({setData}) => {
     const handleSearch = () => {
 
         const fetchLocationData = async() => {
-          await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=f0fe2f7bb81e52b365adc3d4c1738af7`)
+          await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
           .then(res=>{
             if(!res.ok) throw Error(res.statusText)
             return res.json()
@@ -32,18 +34,20 @@ const Search = ({setData}) => {
     <div>
         <div className="search" onKeyPress={handleKeyPress}>
             <input
+            className='s1'
             value={latitude}
             onChange={event => setLatitude(event.target.value)}
-            placeholder='Enter Latitude'
+            placeholder='Latitude'
             type="number" />
 
             <input
+            className='s2'
             value={longitude}
             onChange={event => setLongitude(event.target.value)}
-            placeholder='Enter Longitude'
+            placeholder='Longitude'
             type="number" />
             
-            <button onClick={handleSearch}>Search</button>
+            <FaSearch className='shicon' onClick={handleSearch}/>
         </div>
     </div>
   )
