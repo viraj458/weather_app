27bb7c0d7584fa66aee8d74f6a793b81d6fd8bb3
Viraj <virajmalaka94@gmail.com>
2023-02-25 17:13:16 +0530

    updated WeekCard component

--- src/components/WeekCard.js
+++ src/components/WeekCard.js
@@ -1,24 +1,27 @@
 import React from 'react'
 
+const days = [0,8,16,24,32]
+
 const WeekCard = ({data, back}) => {
   return (
+
+    // shows 5 days temp
     <div className='card'>
-        <div className='today'>
-        {data.list ? <><h1>{data.list[0].main.temp.toFixed()}°C</h1><p>{data.list[0].dt_txt}</p></> : null}
-        </div>
-        <div className='day2'>
-        {data.list ? <><h1>{data.list[8].main.temp.toFixed()}°C</h1><p>{data.list[8].dt_txt}</p></> : null}
-        </div>
-        <div className='day3'>
-        {data.list ? <><h1>{data.list[16].main.temp.toFixed()}°C</h1><p>{data.list[16].dt_txt}</p></> : null}
-        </div>
-        <div className='day4'>
-        {data.list ? <><h1>{data.list[24].main.temp.toFixed()}°C</h1><p>{data.list[24].dt_txt}</p></> : null}
-        </div>
-        <div className='day5'>
-        {data.list ? <><h1>{data.list[32].main.temp.toFixed()}°C</h1><p>{data.list[32].dt_txt}</p></> : null}
-        </div>
-        <button onClick={back}>Back</button>
+      <div className='a'>
+        {days.map((day) => (
+          <div className='week' key={day}>
+            {data.list ? (
+              <>
+                <h1>{data.list[day].main.temp.toFixed()}°C</h1>
+                <p>{data.list[day].dt_txt}</p>
+              </>
+            ) : null}
+            </div>
+        ))}
+      </div>
+      
+      <button onClick={back}>Back</button>
+        
     </div>
   )
 }
