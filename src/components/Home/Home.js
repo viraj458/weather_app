0b4541c889a264b9bdf8142385efe37fd01a3d10
Viraj <virajmalaka94@gmail.com>
2023-02-26 02:23:18 +0530

    created Home.css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,41 @@
 import React from 'react'
+import './Home.css'
 
 const days = [8,16,24];
 
 const Home = ({data, more}) => {
   return (
-    <div className='container'>
+    <div className='cc'>
       {/* shows name of the searched city or default Colombo */}
       <div className='city'>
-      {data.city ? <h1>{data.city.name}</h1> : null}
+      {data.city ? <h2>{data.city.name}</h2> : null}
       </div>
 
       {/* shows current day temp in celcius */}
       <div className='today'>
-      {data.list ? <><h1>{data.list[0].main.temp.toFixed()}°C</h1><p>{data.list[0].dt_txt}</p></> : null}
+      {data.list ? <><h1>{data.list[0].main.temp.toFixed()}°C</h1><p>{data.list[0].dt_txt.split(' ')[0]}</p></> : null}
       </div>
     
       {/* shows next 3 days temp in celcius */}
-      <div className='3days'>
+      <div className='days3'>
+        
         {days.map((day) => (
-          <div className='day2' key={day}>
+          <div className='day' key={day}>
             {data.list ? (
               <>
                 <h1>{data.list[day].main.temp.toFixed()}°C</h1>
-                <p>{data.list[day].dt_txt}</p>
+                <p>{data.list[day].dt_txt.split(' ')[0]}</p>
               </>
             ) : null}
           </div>
         ))}
       </div>
 
-      <button onClick={more}>More</button>
+      <div className='btn'>
+        <button onClick={more}>More</button>
+      </div>
+
+      
 
     </div>
   )
