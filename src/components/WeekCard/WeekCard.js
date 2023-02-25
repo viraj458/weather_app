d937c5bfb7fbf972ae3154995e75da76018c113e
Viraj <virajmalaka94@gmail.com>
2023-02-26 02:24:03 +0530

    created WeekCard.css

--- src/components/WeekCard/WeekCard.js
+++ src/components/WeekCard/WeekCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './WeekCard.css'
 
 const days = [0,8,16,24,32]
 
@@ -13,14 +14,17 @@ const WeekCard = ({data, back}) => {
             {data.list ? (
               <>
                 <h1>{data.list[day].main.temp.toFixed()}°C</h1>
-                <p>{data.list[day].dt_txt}</p>
+                <p>{data.list[day].dt_txt.split(' ')[0]}</p>
               </>
             ) : null}
             </div>
         ))}
       </div>
       
+      <div className='btn2'>
       <button onClick={back}>Back</button>
+      </div>
+      
         
     </div>
   )
