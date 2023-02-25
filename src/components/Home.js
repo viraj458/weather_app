10f62442e14b9a5e596b420058ab28f685d56c39
Viraj <virajmalaka94@gmail.com>
2023-02-25 16:40:54 +0530

    updated Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,31 @@
 import React from 'react'
 
+const num = [8,16,24];
+
 const Home = ({data, more}) => {
   return (
     <div className='container'>
         <div className='city'>
         {data.city ? <h1>{data.city.name}</h1> : null}
         </div>
+
         <div className='today'>
         {data.list ? <><h1>{data.list[0].main.temp.toFixed()}°C</h1><p>{data.list[0].dt_txt}</p></> : null}
         </div>
+
         <div className='3days'>
-          <div className='day2'>
-          {data.list ? <><h1>{data.list[8].main.temp.toFixed()}°C</h1><p>{data.list[8].dt_txt}</p></> : null}
-          </div>
-          <div className='day3'>
-          {data.list ? <><h1>{data.list[16].main.temp.toFixed()}°C</h1><p>{data.list[16].dt_txt}</p></> : null}
-          </div>
-          <div className='day4'>
-          {data.list ? <><h1>{data.list[24].main.temp.toFixed()}°C</h1><p>{data.list[24].dt_txt}</p></> : null}
-          </div>
+          {num.map((n, index) => (
+            <div className='day2' key={index}>
+              {data.list ? (
+                <>
+                  <h1>{data.list[n-1].main.temp.toFixed()}°C</h1>
+                  <p>{data.list[n-1].dt_txt}</p>
+                </>
+              ) : null}
+              </div>
+          ))}
           <button onClick={more}>More</button>
+
         </div>  
       </div>
   )
